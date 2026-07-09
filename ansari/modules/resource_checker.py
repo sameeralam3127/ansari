9e96fdf2edc297e51a34514021cbf0c09d65e654
Sameer Alam <sameeralam3127@gmail.com>
2026-07-09 23:48:11 +0530

    refactor: remove deprecated resource_checker module

--- ansari/modules/resource_checker.py
+++
@@ -1,9 +0,0 @@
-"""Backward-compatible imports for the renamed reliability checker module."""
-
-from ansari.modules.reliability_checker import (  # noqa: F401
-    HealthStatus,
-    ReliabilityChecker,
-    ResourceHealth,
-)
-
-ResourceChecker = ReliabilityChecker
